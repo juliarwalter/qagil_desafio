f1f65acb41bff45a2531f6cb8383ac7dbe4e5ab7
sarahhraamos <sarah17ramos@gmail.com>
2021-03-27 16:10:39 -0300

    subindo codigo

--- src/test/java/io/cucumber/danilo/CasoDeTeste4Steps.java
+++ src/test/java/io/cucumber/danilo/CasoDeTeste4Steps.java
@@ -7,9 +7,21 @@ import io.cucumber.danilo.services.Configuracao;
 
 public class CasoDeTeste4Steps {
 
+    @Dado("clico no menu sobre a accenture")
+    public void clico_no_menu_sobre_a_accenture() {
+        Configuracao.seletorQueryCss("#primaryLink5_SobreaAccenture").click();
+    }
+
     @Dado("clico no item do menu sobre a accenture")
     public void clico_no_item_do_menu_sobre_a_accenture() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Configuracao.seletorQueryCss("Quem somos");
+        
     }
+
+    @Entao("entao devo ver o destaque em {string}")
+    public void entao_devo_ver_o_destaque_em(String string) {
+        Configuracao.seletorQueryCss(".sectionTitle");
+        Configuracao.fechar();
+    }
+
 }
