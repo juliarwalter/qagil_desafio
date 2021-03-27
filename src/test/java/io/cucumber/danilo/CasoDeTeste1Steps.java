3dbf48a71e8924e0de870ab714c783b0113e8580
juliarwalter <ju.rheiwalter@gmail.com>
2021-03-27 12:24:00 -0300

    Ajuste CasoDeTeste1Steps

--- src/test/java/io/cucumber/danilo/CasoDeTeste1Steps.java
+++ src/test/java/io/cucumber/danilo/CasoDeTeste1Steps.java
@@ -25,18 +25,6 @@ public class CasoDeTeste1Steps {
         throw new io.cucumber.java.PendingException();
     }
     
-    @Dado("que eu estou no site da Accenture")
-    public void que_eu_estou_no_site_da_Accenture() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    
-    @Dado("aceito os termos LGPD")
-    public void aceito_os_termos_LGPD() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    
     @Dado("clico em configuracoes de cookie")
     public void clico_em_configuracoes_de_cookie() {
         // Write code here that turns the phrase above into concrete actions
@@ -45,18 +33,6 @@ public class CasoDeTeste1Steps {
     
     @Entao("devo ver o item de {string}")
     public void devo_ver_o_item_de(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    
-    @Entao("devo ver {string}")
-    public void devo_ver(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    
-    @Entao("devo ver {string}")
-    public void devo_ver(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
