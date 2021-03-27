ee0467f256709d668268b09241d09558c6c30470
sarahhraamos <sarah17ramos@gmail.com>
2021-03-27 14:26:11 -0300

    adicionando a classe step do caso de teste 1

--- src/test/java/io/cucumber/danilo/CasoDeTeste1Steps.java
+++ src/test/java/io/cucumber/danilo/CasoDeTeste1Steps.java
@@ -9,33 +9,32 @@ public class CasoDeTeste1Steps {
 
     @Dado("clico em configuracoes de cookie")
     public void clico_em_configuracoes_de_cookie() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Configuracao.seletorQueryCss("#onetrust-pc-btn-handler").click();
     }
-    
+
     @Dado("aceito os termos LGPD")
     public void aceito_os_termos_LGPD() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Configuracao.seletorQueryCss(".save-preference-btn-handler").click();
     }
-    
+
     @Entao("devo fechar a caixa de informacao")
     public void devo_fechar_a_caixa_de_informacao() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    
-    @Entao("devo ver o item de {string}")
-    public void devo_ver_o_item_de(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Configuracao.seletorQueryCss(".banner-close-button").click();
     }
     
     @Entao("devo ver {string}")
     public void devo_ver(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Configuracao.selectorQueryCss("#ot-header-id-1");
+        assertEquals(string, input.getText());
     }
+
+
+    @Entao("devo ver o item de {string}")
+    public void devo_ver_o_item_de(String string) {
+        Configuracao.selectorQueryCss(".category-item");
+        assertEquals(string, input.getText());
+    }
+     
  
 }
   
