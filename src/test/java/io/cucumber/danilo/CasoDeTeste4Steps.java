4f128ac83d5900d919940a280c31a26fd565dd09
juliarwalter <ju.rheiwalter@gmail.com>
2021-03-28 16:50:14 -0300

    CasoDeTeste4Steps: Adicionado os testes; falta alterar: devo_ver_o_destaque_em(String string)

--- src/test/java/io/cucumber/danilo/CasoDeTeste4Steps.java
+++ src/test/java/io/cucumber/danilo/CasoDeTeste4Steps.java
@@ -8,18 +8,20 @@ import io.cucumber.danilo.services.Configuracao;
 public class CasoDeTeste4Steps {
 
     @Dado("clico no menu sobre a accenture")
-    public void clico_no_menu_sobre_a_accenture() {
-        Configuracao.seletorQueryCss("#primaryLink5_SobreaAccenture").click();
+    public void clico_no_menu_sobre_a_accenture() throws InterruptedException {
+        Thread.sleep(2000);
+        Configuracao.seletorQueryXpath("//*[@id='navigation-menu']/div[5]/div[1]/span").click(); 
     }
 
     @Dado("clico no item do menu sobre a accenture")
-    public void clico_no_item_do_menu_sobre_a_accenture() {
-        Configuracao.seletorQueryCss("Quem somos");
+    public void clico_no_item_do_menu_sobre_a_accenture() throws InterruptedException{
+        Thread.sleep(2000);
+        Configuracao.seletorQueryCss("a[href='/br-pt/about/company-index']").click();
         
     }
 
     @Entao("entao devo ver o destaque em {string}")
-    public void entao_devo_ver_o_destaque_em(String string) {
+    public void devo_ver_o_destaque_em(String string) {
         Configuracao.seletorQueryCss(".sectionTitle");
         Configuracao.fechar();
     }
